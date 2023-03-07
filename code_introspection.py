93c2edbef5bd9cfbb3f1e5bc808886726a7d08ad
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-03-07 15:06:17 +0500

    Exercise# 25 - Code Introspection

---
+++ code_introspection.py
@@ -0,0 +1,19 @@
+# Define the Vehicle class.
+
+
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (
+            self.name, self.color, self.kind, self.value)
+        return desc_str
+
+# Print a list of all attributes of the Vehicle class.
+# Your code goes here
+
+
+print(dir(Vehicle))
