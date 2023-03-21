7fdd20e2595213f3d44051a899bea38cb0edc567
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-03-21 16:33:34 +0500

    Weather API ORM Tasks 1, 2, 3 and Bonus

---
+++ WeatherAPIProject/get_current_weather.bat
@@ -0,0 +1,3 @@
+pipenv run python tasks.py --type current --location karachi
+pipenv run python tasks.py --type current --location lahore
+pipenv run python tasks.py --type current --location islamabad
